13e459b80031df252f215f250ea7e40fd9d4d3f0
yuichi <y.y.g.liberty@gmail.com>
2020-11-05 14:10:03 +0900

    dialog表示時にconfirmを出す

--- front/src/components/AddScheduleDialog/container.jsx
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -6,6 +6,7 @@ import {
   addScheduleStartEdit
 } from "../../redux/addSchedule/action";
 import { asyncSchedulesAddItem } from "../../redux/schedules/effects";
+import { isCloseDialog } from "../../services/schedule";
 
 const mapStateToProps = state => ({ schedule: state.addSchedule });
 
@@ -25,16 +26,25 @@ const mapDispatchToProps = dispatch => ({
   }
 });
 
-const mergeProps = (stateProps, dispatchToProps) => ({
-  ...stateProps,
-  ...dispatchToProps,
-  saveSchedule: () => {
-    const {
-      schedule: { form: schedule }
-    } = stateProps;
-    dispatchToProps.saveSchedule(schedule);
-  }
-});
+const mergeProps = (stateProps, dispatchProps) => {
+  const {
+    schedule: { form: schedule }
+  } = stateProps;
+  const { saveSchedule, closeDialog } = dispatchProps;
+
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    saveSchedule: () => {
+      saveSchedule(schedule);
+    },
+    closeDialog: () => {
+      if (isCloseDialog(schedule)) {
+        closeDialog();
+      }
+    }
+  };
+};
 
 export default connect(
   mapStateToProps,
